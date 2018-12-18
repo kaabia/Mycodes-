8bd1ee018b25ca91449798912f9ff9b24548f941
kaabia <badrbacemkaabia@gmail.com>
2018-12-18 15:15:00 +0100

    UART_DMA_RX

---
+++ 08_UART_DMA_RX/MDK-ARM/08_UART_DMA_RX/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+08_uart_dma_rx\startup_stm32f407xx.o: startup_stm32f407xx.s
---
+++ 08_UART_DMA_RX/MDK-ARM/RTE/_08_UART_DMA_RX/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '08_UART_DMA_RX' 
+ * Target:  '08_UART_DMA_RX' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
