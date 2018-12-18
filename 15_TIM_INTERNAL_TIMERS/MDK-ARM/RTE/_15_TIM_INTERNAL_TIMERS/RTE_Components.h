a4cec234e0217517453d02b31e916d8809f762a8
kaabia <badrbacemkaabia@gmail.com>
2018-12-18 15:56:48 +0100

    timer_internal

---
+++ 15_TIM_INTERNAL_TIMERS/MDK-ARM/RTE/_15_TIM_INTERNAL_TIMERS/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '15_TIM_INTERNAL_TIMERS' 
+ * Target:  '15_TIM_INTERNAL_TIMERS' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
